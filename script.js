bb9bb71ac244f62c1de36108b2b83e59ac285410
eaglehaast <eaglehaast@gmail.com>
2025-10-20 13:26:28 +0400

    Adjust score counter vertical offset

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ const SCORE_COUNTER_ELEMENTS = {
 };
 
 const SCORE_COUNTER_BASE_OFFSETS = {
-  green: { x: 4,   y: 288 },
-  blue:  { x: 414, y: 288 }
+  green: { x: 4,   y: 388 },
+  blue:  { x: 367, y: 388 }
 };
 
 const SCORE_COUNTER_BASE_SIZE = { width: 43, height: 124 };
