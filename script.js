7519275d2f9d6916d508c0528326be222bc660d3
eaglehaast <eaglehaast@gmail.com>
2025-12-14 00:00:13 +0400

    Revert "Temporarily disable plane glow highlights"

--- script.js
+++ script.js
@@ -4742,8 +4742,7 @@ function drawPlanesAndTrajectories(){
 
   let rangeTextInfo = null;
   const activeColor = turnColors[turnIndex];
-  // Temporarily disable all plane glow/highlight effects for debugging.
-  const showGlow = false;
+  const showGlow = !handleCircle.active && !flyingPoints.some(fp => fp.plane.color === activeColor);
   const destroyedOrBurning = [];
   const activePlanes = [];
 
@@ -4774,9 +4773,15 @@ function drawPlanesAndTrajectories(){
 
     // Allow wreck sprites to render after explosions finish instead of exiting early.
     drawPlaneSegments(targetCtx, p);
-    const glowTarget = 0;
-    p.glow = 0;
-    drawThinPlane(targetCtx, p, glowTarget);
+    const glowTarget = showGlow && p.color === activeColor && p.isAlive && !p.burning ? 1 : 0;
+    if(p.glow === undefined) p.glow = glowTarget;
+    if(!p.isAlive || p.burning){
+      p.glow = 0;
+    } else {
+      p.glow += (glowTarget - p.glow) * 0.1;
+    }
+    const renderGlow = (!p.isAlive || p.burning) ? 0 : p.glow;
+    drawThinPlane(targetCtx, p, renderGlow);
 
     if(allowRangeLabel && handleCircle.active && handleCircle.pointRef === p){
       let vdx = handleCircle.shakyX - p.x;
