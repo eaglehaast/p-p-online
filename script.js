c6ae578441981649888d9e210cd16f09c19ba3b9
eaglehaast <eaglehaast@gmail.com>
2025-12-14 16:58:55 +0400

    Revert "Change hostRect declaration to const"

--- script.js
+++ script.js
@@ -935,13 +935,11 @@ function resolvePlaneFlameMetrics(context = 'plane flame') {
     return null;
   }
 
-const hostRect = getViewportAdjustedBoundingClientRect(host);
-if (!hostRect || hostRect.width <= FX_HOST_MIN_SIZE || hostRect.height <= FX_HOST_MIN_SIZE) {
-  console.warn(`[FX] Skipping ${context}: host rect invalid`, { hostRect });
-  return null;
-
-}
-
+  const hostRect = getViewportAdjustedBoundingClientRect(host);
+  if (!hostRect || hostRect.width <= FX_HOST_MIN_SIZE || hostRect.height <= FX_HOST_MIN_SIZE) {
+    console.warn(`[FX] Skipping ${context}: host rect invalid`, { hostRect });
+    return null;
+  }
 
   const usableBoardRect = (boardRect && boardRect.width > FX_HOST_MIN_SIZE && boardRect.height > FX_HOST_MIN_SIZE)
     ? boardRect
